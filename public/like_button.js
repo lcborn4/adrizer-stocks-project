e8ed5729fec481b91a6915cab2985a7b9c55631f
Logan Born <lcborn4@gmail.com>
2020-07-29 00:17:32 -0400

    jsx input

--- public/like_button.js
+++ public/like_button.js
@@ -1,32 +1,66 @@
-'use strict';
+// 'use strict';
 
-const e = React.createElement;
+// const e = React.createElement;
 
-class LikeButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { liked: false };
-    }
+// class LikeButton extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { liked: false };
+//     }
 
-    render() {
-        if (this.state.liked) {
-            return e(
-                'button',
-                { onClick: () => this.setState({ liked: false }) },
-                'unlike'
-            );
-        }
-        else {
+//     render() {
+//         if (this.state.liked) {
+//             return e(
+//                 'button',
+//                 { onClick: () => this.setState({ liked: false }) },
+//                 'unlike'
+//             );
+//         }
+//         else {
 
-            return e(
-                'button',
-                { onClick: () => this.setState({ liked: true }) },
-                'Like'
-            );
+//             return e(
+//                 'button',
+//                 { onClick: () => this.setState({ liked: true }) },
+//                 'Like'
+//             );
 
-        }
-    }
+//         }
+//     }
+// }
+
+// const domContainer = document.querySelector('#root');
+// ReactDOM.render(e(LikeButton), domContainer);
+
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    //debug
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
-
-    const domContainer = document.querySelector('#like_button_container');
-    ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(<NameForm />, document.getElementById('root'));
