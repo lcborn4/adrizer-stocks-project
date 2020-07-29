7fcf22f011f5815c0646af4c45e270a6154e457c
Logan Born <lcborn4@gmail.com>
2020-07-28 22:47:54 -0400

    unlike feature

--- public/like_button.js
+++ public/like_button.js
@@ -3,24 +3,30 @@
 const e = React.createElement;
 
 class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
     }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
+    render() {
+        if (this.state.liked) {
+            return e(
+                'button',
+                { onClick: () => this.setState({ liked: false }) },
+                'unlike'
+            );
+        }
+        else {
+
+            return e(
+                'button',
+                { onClick: () => this.setState({ liked: true }) },
+                'Like'
+            );
+
+        }
+    }
 }
 
-
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+    const domContainer = document.querySelector('#like_button_container');
+    ReactDOM.render(e(LikeButton), domContainer);
